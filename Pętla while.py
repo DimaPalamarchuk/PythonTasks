215f2a4426e498ccf04b0bcf8f66c5c82f6c3f91
w68161 <dimapalamarchuk310@gmail.com>
2022-10-25 12:31:50 +0200

    Laboratorium

--- Pętla while.py
+++ Pętla while.py
@@ -1,9 +1,7 @@
-a = int(input('Podaj liczbe a: '))
-b = int(input('Podaj liczbe b: '))
-
+a = int(input("podaj liczbę a: "))
+b = int(input("podaj liczbę b: "))
 if b<a:
     a,b=b,a
-
 while b>=a:
-    print(a, end='')
+    print(a,end=' ')
     a=a+1
