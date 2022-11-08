6266a45809e28d7d6b1ca7aa08482f4a69a7da0e
w68161 <dimapalamarchuk310@gmail.com>
2022-11-08 13:14:59 +0100

    Lab. 3

--- Zadanie 5. Instrukcje break, continue, pass w pętlach.py
+++ Zadanie 5. Instrukcje break, continue, pass w pętlach.py
@@ -1,8 +1,11 @@
 n = int(input('Studentów = '))
-x = int(input(''))
+a = 1
 suma = 0
 while n:
-    n =
-    punkty = int(input(''))
-    suma+=punkty
-    print(punkty)
+    b=int(input(f'Podaj liczbę punktów studenta {a} :'))
+    if (b<0 or b>100):
+        continue
+    suma+=b
+    a+=1
+
+else: print(suma/n)
---
+++ Zadanie 7.py
@@ -0,0 +1,3 @@
+print()
+for number in range(20, -2, -2)
+    print(number, end='')
